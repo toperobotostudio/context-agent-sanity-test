298af54ddefd8ad5d71e1816b3baf86e7f02ab17
tope <tope@robotostudio.com>
2026-03-04 16:33:46 +0100

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+import { withBotId } from "botid/next/config";
+
+const nextConfig: NextConfig = {
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatar.vercel.sh",
+      },
+      {
+        protocol: "https",
+        //https://nextjs.org/docs/messages/next-image-unconfigured-host
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
+
+export default withBotId(nextConfig);
